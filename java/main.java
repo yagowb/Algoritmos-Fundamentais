58a6ff8c4054b2bfcc8e9118d73615a9eb58b175
yago rodrigues <yagodev3@gmail.com>
2022-08-05 18:28:48 -0300

    adição de algoritmos

---
+++ java/main.java
@@ -0,0 +1,23 @@
+class Main {
+    public static void main(String[] args){
+
+
+    //TrocaDeVariaveis t = new TrocaDeVariaveis();
+    //t.TrocaDeValores();
+
+    //Contagem t = new Contagem();
+    //t.Contagem();
+
+    //Somatorio t = new Somatorio();
+    //t.Somatorio();
+
+    //NumeroPrimo t = new NumeroPrimo();
+    //t.NumeroPrimo();
+
+    //MaximoDivisorComum t = new MaximoDivisorComum();
+    //t.MaximoDivisorComum();
+
+    //Fibonacci t = new Fibonacci();
+    //t.Fibonacci();
+    }
+}
