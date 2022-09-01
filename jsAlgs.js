8c2ae59b972cc3c8da9bddd9bd0f04af1fc381e7
yago rodrigues <yagodev3@gmail.com>
2022-09-01 18:57:08 -0300

    Adição de boodstrap+ html dos algoritmos + ajustes

---
+++ jsAlgs.js
@@ -0,0 +1,14 @@
+const sumNum1Selector = document.getElementById('sum-num1');
+const sumNum2Selector = document.getElementById('sum-num2');
+const sumResultSelector = document.getElementById('sum-result');
+const sumBtn = document.getElementById('sum-btn');
+
+function sum () {
+  console.log('---SUM CALLED?---')
+  // '1'
+  // +'1'
+  // 1
+  sumResultSelector.value = +sumNum1Selector.value + +sumNum2Selector.value;
+}
+
+sumBtn.addEventListener('click', sum);
