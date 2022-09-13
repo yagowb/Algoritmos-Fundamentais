b4c5c23131d9d349b302b03bddd539ea195f0503
yago rodrigues <yagodev3@gmail.com>
2022-09-13 20:59:43 -0300

    Melhoria nos códigos Java

---
+++ java/Fatorial.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Fatorial{
+    public static void main(String[] args) {
+        Scanner prompt = new Scanner(System.in);
+        int n, prod = 1, i = 0;
+
+        System.out.println("Digite um número para verificar seu fatorial: ");
+        n = prompt.nextInt();
+
+        while(i<n){
+            i = i + 1;
+            prod = prod * i;
+        }
+        System.out.println("O fatorial de " + n + " é igual a: " + prod);
+    }
+}
