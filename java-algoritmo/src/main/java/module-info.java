e6ab4a3852521e70c770b2f6f7a2288c6ed1e563
yago rodrigues <yagodev3@gmail.com>
2022-10-11 20:27:52 -0300

    Alterações código JS e ajustes CSS

--- java-algoritmo/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.javaalgoritmo {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.javaalgoritmo to javafx.fxml;
-    exports com.example.javaalgoritmo;
-}
